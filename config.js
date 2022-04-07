3e3a17c55d11534490350aba13b15a4ace1c4d73
Srivats Viswanathan <77873544+Srivats728@users.noreply.github.com>
2022-04-07 17:04:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 const config = {
-    token: 'ODM4NjQwNDY1NjQ4OTQzMTc3.YI-CzA.ScIZSBAN_UAHHHCGS7iltmLX89E',
-    prefix: '!om'
+    token: 'yourtokenhere',
+    prefix: '!'
 }
-////واظغط RUN
-//اشتراك ولايك 100 لايك انزل الاصدار المطور
 module.exports = config;
